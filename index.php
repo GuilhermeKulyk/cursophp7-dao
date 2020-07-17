06252a84b049bd211c6a13fece961dfb5ea69ca4
Guilherme Kulik <guilhermekulik@gmail.com>
2020-07-16 23:04:49 -0300

    Metodos de listagem

--- index.php
+++ index.php
@@ -2,15 +2,24 @@
 require_once("config.php");
 //require_once("Sql.php");
 //$sql = new Sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
-$root = new Usuario();
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
+//carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search('Usuario');
+//echo json_encode($search);
 
-echo $root;
+//carrega uma lista
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//CARREGA UM USUÁRIO USANDO LOGIN E SENHA
+$usuario = new Usuario();
+$usuario->login("Usuario", "teste");
+
+echo $usuario;
 ?>
